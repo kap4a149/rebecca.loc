af3d2e030b87c5477b011415adffca0a7f3aed42
kap4a149 <36609566+kap4a149@users.noreply.github.com>
2018-05-26 00:57:52 +0300

    added timezone

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-//session_start();
-//unset($_SESSION['auth']);
-//session_destroy();
-ini_set('error_reporting', E_ALL); ini_set('display_errors', 1); ini_set('display_startup_errors', 1);
-
-
-//setcookie('access', '1', time()+60000*10000);
-if($_COOKIE['access'] !== '1'){
-    die('Новая система совсем скоро. <br /><b>Пары 02.05.2018:</b> <br/> 1. Физкультура<br />2.Численные методы (409ф)<br />3.ТКП (609ф)');
-}
-
-//require_once 'pages/signin.php';
+date_default_timezone_set("Europe/Kiev");
 require_once 'pages/mainPage.php';
 ?>
